4136de950fe1359dae9f98f50d845125ab13517f
DREdward-chell <sushkov2007@gmail.com>
2024-03-28 14:58:03 +1000

    Example bot. Can save information about local session and is able to save it in JSON

--- bot.js
+++ bot.js
@@ -1,11 +1,32 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf, Router, Context, Markup } = require("telegraf");
+const LocalSession = require("telegraf-session-local");
 const { message } = require("telegraf/filters");
 
 const bot = new Telegraf("7044967471:AAE2LjXlasCi2DCjFTQ8qej9mZK7vNcKFGs");
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+const session = new LocalSession('sessions.json');
+bot.use(session.middleware())
+
+bot.start((ctx) => {
+    if (!ctx.session.userData) {
+        ctx.session.userData = {};
+        ctx.session.userData['local'] = String(Math.floor(Math.random() * 100));
+        ctx.reply(
+            'Your number is ' + ctx.session.userData['local'],
+            Markup.inlineKeyboard([
+                {text: 'ok', callback_data: '1'}
+            ])
+        )
+    } else {
+        ctx.reply('Bot is ready!')
+    }
+});
+
+bot.action('1', (ctx) => {
+    ctx.deleteMessage();
+})
+
+bot.command("number", (ctx) => ctx.reply(ctx.session.userData['local']));
+
 bot.launch();
 
 // Enable graceful stop
