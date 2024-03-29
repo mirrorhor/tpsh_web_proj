e17f0826eab405d8cab359e94731bdabbd0f8128
DREdward-chell <sushkov2007@gmail.com>
2024-03-29 15:48:40 +1000

    Test bot. Use '/register' to open webview

--- bot.js
+++ bot.js
@@ -1,34 +1,33 @@
-const { Telegraf, Router, Context, Markup } = require("telegraf");
-const LocalSession = require("telegraf-session-local");
-const { message } = require("telegraf/filters");
+const { Telegraf, Router, Context, Markup } = require('telegraf');
+const LocalSession = require('telegraf-session-local');
+const { message } = require('telegraf/filters');
+const express = require('express');
 
-const bot = new Telegraf("7044967471:AAE2LjXlasCi2DCjFTQ8qej9mZK7vNcKFGs");
+
+const bot = new Telegraf('7044967471:AAE2LjXlasCi2DCjFTQ8qej9mZK7vNcKFGs');
 const session = new LocalSession('sessions.json');
+var context = 0
+bot.start((ctx) => {context = ctx.chat.id; console.log(context)})
 bot.use(session.middleware())
 
-bot.start((ctx) => {
-    if (!ctx.session.userData) {
-        ctx.session.userData = {};
-        ctx.session.userData['local'] = String(Math.floor(Math.random() * 100));
-        ctx.reply(
-            'Your number is ' + ctx.session.userData['local'],
-            Markup.inlineKeyboard([
-                {text: 'ok', callback_data: '1'}
-            ])
-        )
-    } else {
-        ctx.reply('Bot is ready!')
-    }
+bot.command("register", (ctx) => {
+  return ctx.reply(
+    "open webapp",
+    Markup.keyboard([
+      Markup.button.webApp(
+        "Open",
+        "https://tg-bot-web-app-pps.vercel.app/"
+      )
+    ])
+  );
+});
+bot.on("message", async (ctx) => {
+  console.log(ctx.message.web_app_data)
+  return ctx.reply(ctx.message.web_app_data.data)
+
 });
 
-bot.action('1', (ctx) => {
-    ctx.deleteMessage();
-})
-
-bot.command("number", (ctx) => ctx.reply(ctx.session.userData['local']));
-
 bot.launch();
-
 // Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
